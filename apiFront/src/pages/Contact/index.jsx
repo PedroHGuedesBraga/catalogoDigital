e766ec5b57c6dc343e5a2de5b09b9638dd6592a7
Pedro Henrique Guedes <pedroguedes58@hotmail.com>
2023-11-29 07:54:33 -0300

    login/contact

--- apiFront/src/pages/Contact/index.jsx
+++ apiFront/src/pages/Contact/index.jsx
@@ -1,12 +1,24 @@
 import Header from "../../components/Header"
 import Footer from '../../components/Footer'
 import Container from "../../components/Container"
+import styles from "./Contact.module.css"
+import { FaWhatsapp , FaInstagram  } from "react-icons/fa";
+
 function Contact() {
     return (
         <>
             <Header />
             <Container>
-                <h2>Contact</h2>
+                <section className={styles.contact}>
+                    <h2>Gostou de um dos nossos produtos ?</h2>
+                    <h3>Entre em contato conosco por um de nossos links !</h3>
+                    <div className={styles.icons}> 
+                    <a href="https://wa.me/5583991217070?text=Ol%C3%A1%20gostaria%20de%20adquirir%20um%20produto"><FaWhatsapp className={styles.iconW} /></a>
+                    <a href="https://www.instagram.com/eipmake/"><FaInstagram className={styles.iconI} /></a>
+                        
+                        
+                    </div>
+                </section>
             </Container>
             <Footer />
         </>
--- apiFront/src/pages/Login/index.jsx
+++ apiFront/src/pages/Login/index.jsx
@@ -1,14 +1,52 @@
 import Header from "../../components/Header"
 import Footer from '../../components/Footer'
 import Container from "../../components/Container"
+import React, { useState, Button } from 'react';
+import styles from './Login.module.css';
+
+
 function Login() {
+    const [login, setLogin] = useState('');
+    const [senha, setSenha] = useState('');
+
+    // Manipuladores de eventos para atualizar os estados
+    const handleLoginChange = (event) => {
+        setLogin(event.target.value);
+    };
+
+    const handleSenhaChange = (event) => {
+        setSenha(event.target.value);
+    };
+
+    // Manipulador de evento para lidar com o envio do formulário (pode adicionar lógica de autenticação aqui)
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('Login:', login, 'Senha:', senha);
+        // Adicione aqui a lógica de autenticação, por exemplo, uma chamada de API para verificar credenciais
+    };
     return (
         <>
-        <Header />
+            <Header />
             <Container>
-                <h1>Login</h1>
+                <div className={styles.login}>
+                    <form onSubmit={handleSubmit}>
+                        <label>
+                            <h3>Login</h3>
+                            <input className={styles.input} type="text" value={login} onChange={handleLoginChange} />
+                        </label>
+                        <br />
+                        <label>
+                            <h3>Senha</h3>
+                            <input className={styles.input} type="password" value={senha} onChange={handleSenhaChange} />
+                        </label>
+                        <br />
+                        <br />
+                        <button className={styles.button} type="submit">Login</button>
+                      
+                    </form>
+                </div>
             </Container>
-        <Footer/>
+            <Footer />
         </>
 
     )
