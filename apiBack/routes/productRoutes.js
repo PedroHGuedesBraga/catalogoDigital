6e157e893ee8c6b3226b0016a0758a3b41335d54
Pedro Henrique Guedes <pedroguedes58@hotmail.com>
2023-11-29 11:49:24 -0300

    css

--- apiBack/routes/productRoutes.js
+++ apiBack/routes/productRoutes.js
@@ -5,8 +5,8 @@ const productController = require("../controllers/productController");
 const authenticateUserJWT = require('../middleware/authMid');
 
 router.route("/product").post(authenticateUserJWT,(req,res)=>productController.create(req,res));
-router.route("/product").get(authenticateUserJWT,(req,res)=>productController.getAll(req,res));
-router.route("/product/:id").get(authenticateUserJWT,(req,res)=>productController.get(req,res));
+router.route("/product").get((req,res)=>productController.getAll(req,res));
+router.route("/product/:id").get((req,res)=>productController.get(req,res));
 router.route("/product/:id").delete(authenticateUserJWT,(req,res)=>productController.delete(req,res));
 router.route("/product/:id").put(authenticateUserJWT,(req,res)=>productController.update(req,res));
 
